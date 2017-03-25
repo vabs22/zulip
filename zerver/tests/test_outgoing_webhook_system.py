ae3083e9802dde939b7c761578a442c72b97b59e
vaibhav <singhalvaibhav28@gmail.com>
2017-03-25 23:44:35 +0530

    Outgoing Webhook System: Add support for personal message triggers.

--- zerver/tests/test_outgoing_webhook_system.py
+++ zerver/tests/test_outgoing_webhook_system.py
@@ -3,16 +3,17 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import mock
-from typing import Any
+from typing import Any, Union, Text, List, Mapping, Callable
 
 from zerver.lib.outgoing_webhook import (
     do_rest_call,
     get_outgoing_webhook_service_handler,
     ServiceMessageActions,
 )
+from zerver.lib.actions import check_send_message
 from zerver.lib.test_helpers import get_user_profile_by_email
 from zerver.lib.test_classes import ZulipTestCase
-from zerver.models import Services
+from zerver.models import Services, Recipient, Client, UserProfile, get_realm, Subscription
 from zerver.outgoing_webhooks import (
     GENERIC_INTERFACE,
     AVAILABLE_OUTGOING_WEBHOOK_INTERFACES,
@@ -20,6 +21,7 @@ from zerver.outgoing_webhooks import (
 )
 
 import requests
+import six
 
 service = Services.objects.create(service_name="test",
                                   base_url="http://requestb.in/18av3gx1",
@@ -117,3 +119,74 @@ class OutgoingWebhookServiceHandlerTests(ZulipTestCase):
         self.assertEqual(service_handler.service_api_key, generic_interface.service_api_key)
         self.assertEqual(service_handler.bot_email, generic_interface.bot_email)
         self.assertEqual(service_handler.service_name, generic_interface.service_name)
+
+
+class PersonalMessageTriggersTest(ZulipTestCase):
+
+    @classmethod
+    def setUpClass(self):
+        # type: () -> None
+        self.bot_user = UserProfile.objects.create(full_name='The Test Bot',
+                                                   short_name='bot',
+                                                   email="testvabs-bot@zulip.com",
+                                                   is_bot=True,
+                                                   bot_type=UserProfile.OUTGOING_WEBHOOK_BOT,
+                                                   is_active=True,
+                                                   pointer=1,
+                                                   realm=get_realm("zulip"))
+
+        self.recipient = Recipient.objects.create(type=Recipient.PERSONAL,
+                                                  type_id=self.bot_user.id)
+
+        self.subscription = Subscription.objects.create(recipient=self.recipient,
+                                                        user_profile=self.bot_user,
+                                                        active=True)
+
+    @classmethod
+    def tearDownClass(self):
+        # type: () -> None
+        self.bot_user.delete()
+        self.recipient.delete()
+        self.subscription.delete()
+
+    def test_trigger_on_stream_message(self):
+        # type: () -> None
+        sender_email = "othello@zulip.com"
+        recipients = "Denmark"
+        message_type = Recipient.STREAM
+        with mock.patch('zerver.lib.actions.queue_json_publish') as queue_json_publish:
+            self.send_message(sender_email, recipients, message_type)
+            self.assertFalse(queue_json_publish.called)
+
+    def test_trigger_on_private_message_by_bot(self):
+        # type: () -> None
+        sender_email = "testvabs-bot@zulip.com"
+        recipients = "othello@zulip.com"
+        message_type = Recipient.PERSONAL
+
+        with mock.patch('zerver.lib.actions.queue_json_publish') as queue_json_publish:
+            self.send_message(sender_email, recipients, message_type)
+            self.assertFalse(queue_json_publish.called)
+
+    @mock.patch('zerver.lib.actions.queue_json_publish')
+    def test_trigger_on_private_message_by_user(self, mock_queue_json_publish):
+        # type: (mock.Mock) -> None
+        sender_email = "othello@zulip.com"
+        recipients = "testvabs-bot@zulip.com"
+        message_type = Recipient.PERSONAL
+
+        def check_values_passed(queue_name, trigger_event, x):
+            # type: (Any, Union[Mapping[Any, Any], Any], Callable[[Any], None]) -> None
+            self.assertEqual(queue_name, "outgoing_webhooks")
+            self.assertEqual(trigger_event['bot_email'], recipients)
+            self.assertEqual(trigger_event["command"], "test")
+            self.assertEqual(trigger_event["retry"], 0)
+            self.assertEqual(trigger_event["service_name"], None)
+            self.assertEqual(trigger_event["message"]["sender_email"], sender_email)
+            self.assertEqual(trigger_event["message"]["type"], self.recipient.type_name())
+            self.assertEqual(trigger_event["message"]["sender_realm_str"], self.bot_user.realm.string_id)
+            self.assertEqual(trigger_event["message"]["recipient"][0]["email"], recipients)
+
+        mock_queue_json_publish.side_effect = check_values_passed
+        self.send_message(sender_email, recipients, message_type, subject='', content='test')
+        self.assertTrue(mock_queue_json_publish.called)
